8c845cb94c460353e9990d5e60a6ae31540b2d92
nazoinu <albizia.tk@gmail.com>
2023-01-09 08:58:31 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -30,10 +30,10 @@ class OwnersController extends Controller
         echo '-------------';
         echo $data_parse;
         */
-        
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        //$q_get = DB::table('owners')->select('name', 'created_at')->get();
+
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -43,7 +43,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
